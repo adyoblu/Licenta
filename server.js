9762461c248b17e356082617ee1729636063b17e
adyoblu <adyoblu2000@yahoo.com>
2020-09-08 18:04:38 +0300

    another try

--- server.js
+++ server.js
@@ -3,28 +3,37 @@ var jsonServer = require('json-server'),
     router = jsonServer.router('db.json');
 
 
-var cons = require('consolidate'),
-    path = require('path'),
-    bodyParser = require("body-parser"),
-    express = require('express');
+// var cons = require('consolidate'),
+//     path = require('path'),
+//     bodyParser = require("body-parser"),
+//     express = require('express'),
+//     flash = require('connect-flash');
 
 var middlewares = jsonServer.defaults({static: '/public'});
 
-app.get('/', function(req,res,next){ 
-    res.render('index');
-});
+// app.get('/', function(req,res,next){ 
+//     res.render('index.html');
+// });
 
 var port = process.env.PORT || 3000;
 
-    app.engine('html', cons.swig);
-    app.set('views', path.join(__dirname, 'views'));
-    app.set('view engine', 'html');
-    app.use(bodyParser.urlencoded({extended: true}));
-    app.use(router);
-    app.use(middlewares);
-    app.use(express.static(path.join(__dirname, 'public')));
-    app.use('public/javascripts', express.static(path.join(__dirname, 'public/scripts')));
-    app.use('public/stylesheets', express.static(path.join(__dirname, 'public/stylesheets')));
+
+// app.use(require("express-session")({
+//     secret: "Shome",
+//     resave: false,
+//     saveUninitialized: false
+// }));
+
+//     app.set('views', path.join(__dirname, 'views'));
+//     app.engine('html', cons.swig);
+//     app.set('view engine', 'html');
+//     app.use(bodyParser.urlencoded({extended: true}));
+//     app.use(router);
+//     app.use(middlewares);
+//     app.use(express.static(__dirname + "/public"));
+//     app.use('public/javascripts', express.static(path.join(__dirname, 'public/scripts')));
+//     app.use('public/stylesheets', express.static(path.join(__dirname, 'public/stylesheets')));
+//     app.use(flash());
 
 
 app.use(middlewares);
